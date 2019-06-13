93a8827d8f56981172d3c4e76a0b0676fc6f075c
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2019-06-13 11:35:22 +0300

    feat(delay): add delay pipe

--- projects/async-pipeline/src/lib/async-pipeline.module.ts
+++ projects/async-pipeline/src/lib/async-pipeline.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { NotPipe } from './pipes/not.pipe';
 import { LengthPipe } from './pipes/length.pipe';
+import { DelayPipe } from './pipes/delay.pipe';
 
 @NgModule({
   declarations: [
     NotPipe,
     LengthPipe,
+    DelayPipe,
   ],
 })
 export class AsyncPipelineModule {
